1a3998e79b6d5948b768586599f007dab6f82e0e
Ghadeer Elsalhawy <54215096+ghadeer-elsalhawy@users.noreply.github.com>
2022-10-28 20:24:12 +0200

    Implement get_token_auth_header()

---
+++ Problems Faced.md
@@ -0,0 +1,5 @@
+### Running the app
+
+- cd to /backend/src
+- Run the following cmmand in the server: `$env:FLASK_APP="api.py"`
+- Run `flask run`
--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -4,7 +4,6 @@ from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
 
-
 AUTH0_DOMAIN = 'udacity-fsnd.auth0.com'
 ALGORITHMS = ['RS256']
 API_AUDIENCE = 'dev'
@@ -31,7 +30,36 @@ class AuthError(Exception):
     return the token part of the header
 '''
 def get_token_auth_header():
-   raise Exception('Not Implemented')
+    auth = request.headers.get('Authorization', None)
+    if not auth:
+        raise AuthError({
+            'code': 'authorization_header_missing',
+            'description': 'Authorization header is expected.'
+        }, 401)
+    
+    parts = auth.split()
+    if parts[0].lower() != 'bearer':
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization header must start with "Bearer".'
+        }, 401)
+
+    elif len(parts) == 1:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Token not found.'
+        }, 401)
+    
+    elif len(parts) > 2:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization header must be bearer token.'
+        }, 401)
+
+    token = parts[1]
+    return token
+    
+    # Done
 
 '''
 @TODO implement check_permissions(permission, payload) method
